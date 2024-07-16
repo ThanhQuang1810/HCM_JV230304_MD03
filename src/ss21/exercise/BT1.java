2ae09f2426a083be9fbe7554ba36af7c38b9ca88
ThanhQuang1810 <nguyentranthanhquang1810@gmail.com>
2024-07-16 10:23:33 +0700

    update

---
+++ src/ss21/exercise/BT1.java
@@ -0,0 +1,19 @@
+package src.ss21.exercise;
+
+public class BT1 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            try {
+                for (int i = 10; i > 0; i--) {
+                    System.out.println(i);
+                    Thread.sleep(1000);
+                }
+                System.out.println("Kết thúc");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+
+    }
+}
